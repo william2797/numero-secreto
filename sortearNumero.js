5b595be82a5eb26224577ff6f9ac2a8553ae11c4
William Souza <williamd400@gmail.com>
2023-11-08 12:25:14 -0300

    commit index.html

---
+++ sortearNumero.js
@@ -0,0 +1,20 @@
+const menorValor = 1
+const maiorValor = 1000
+const numeroSecreto  = geraNumeroAleatorio()
+
+
+
+
+function geraNumeroAleatorio() {
+    return parseInt(Math.random() * maiorValor + 1)
+} 
+
+console.log(numeroSecreto)
+
+
+
+const elementoMenorValor = document.getElementById('menor-valor')
+elementoMenorValor.innerHTML = menorValor
+
+const elementoMaiorValor = doccument.getElementById('maior-valor')
+elementoMaiorValor.innerHTML = maiorValor
